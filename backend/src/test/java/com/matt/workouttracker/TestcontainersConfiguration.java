bba688c504771252b51028b8deb86371b7cf1b98
Matthew <mrideout1985@gmail.com>
2026-08-10 21:42:30 +0100

    chore: added test containers

---
+++ backend/src/test/java/com/matt/workouttracker/TestcontainersConfiguration.java
@@ -0,0 +1,16 @@
+package com.matt.workouttracker;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:16");
+    }
+}
--- backend/src/test/java/com/matt/workouttracker/WorkouttrackerApplicationTests.java
+++ backend/src/test/java/com/matt/workouttracker/WorkouttrackerApplicationTests.java
@@ -2,8 +2,10 @@ package com.matt.workouttracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class WorkouttrackerApplicationTests {
 
 	@Test
